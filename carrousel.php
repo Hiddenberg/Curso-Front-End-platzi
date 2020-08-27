e6b46e6a78fbe5178b4608fa01c1ad1354e6f06b
Victor Pazaran <victor_hals@hotmail.com>
2020-08-26 20:52:34 -0500

    implementando carrusel con carga automatica de imagenes en php

---
+++ carrousel.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>Carrousel</title>
+   <link rel="stylesheet" href="carrouse-style.css">
+</head>
+<body>
+   <section class="carousel">
+      <div class="carousel__container">
+         <?php
+         $images = scandir("./carousel-img");
+         foreach ($images as $image) {
+            if ($image != "." and $image != "..") {
+               echo "
+                  <div class='carousel-item'>
+                  <img class='carousel-item-img' src='./carousel-img/$image'>;
+                  </div>
+                  ";
+            }
+         }
+         ?>
+      </div>
+   </section>
+</body>
+</html>
